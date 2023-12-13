3e17b5653d277b3e089403e4247983139ea401f9
Zahra121377 <afkhami121377@gmail.com>
2023-12-14 00:58:30 +0330

    Add a simple view to watch list of doctors

--- healthcare/views.py
+++ healthcare/views.py
@@ -1,1 +1,6 @@
 # Create your views here.
+from django.shortcuts import render
+from .models import Doctor
+def doctors_list(request):
+    doctors = Doctor.objects.all()
+    return render(request, 'doctor.html', {'doctors': doctors})
