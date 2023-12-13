b5acbc0e0f5ee32eec0abc516810162e66ddf802
Zahra121377 <afkhami121377@gmail.com>
2023-12-14 00:44:59 +0330

    Add Doctor model to app

--- healthcare/models.py
+++ healthcare/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Doctor(models.Model):
+    name = models.CharField(max_length=50)
+    specialization = models.CharField(max_length=100)
+    profile_image = models.ImageField(upload_to='doctor_profile_images/')
+    facebook_link = models.URLField(blank=True, null=True)
+    twitter_link = models.URLField(blank=True, null=True)
+    linkedin_link = models.URLField(blank=True, null=True)
+    instagram_link = models.URLField(blank=True, null=True)
