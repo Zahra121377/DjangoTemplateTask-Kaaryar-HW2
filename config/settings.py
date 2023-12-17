1e7c0f81e13f9978413d95a48f882e24ed32246e
Zahra121377 <afkhami121377@gmail.com>
2023-12-17 14:37:53 +0330

    Add template again in a correct way

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,20 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+# settings.py
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = "static/"
+
+STATIC_ROOT = BASE_DIR / "static/"
+
+STATICFILES_DIRS = [BASE_DIR / "config/static/"]
+
+
+
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
